da019a5a143757f8440eaa08c9e38c5c679acfda
Matthieu Carrara <matthieu6019@gmail.com>
2022-02-07 17:04:54 +0100

    images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,19 +10,19 @@ export default function Portfolio() {
                  <div className="container">
                      <div className="item">
                          <a href="https://diabloows.github.io/APA-Association-Protection-Animali-re" target="_blank" rel="noreferrer">
-                             <img alt="" src="images/APA.jpg" />
+                             <img alt="" src="images/apa1.jpeg" />
                              <h3>APA Website HTML CSS</h3>
                          </a>
                      </div>
                      <div className="item">
                          <a href="https://diabloows.github.io/V3-Tout-ou-Rien" target="_blank" rel="noreferrer">
-                             <img alt="" src="images/DICEGAME2.jpg" />
+                             <img alt="" src="images/dice3.jpeg" />
                              <h3>Marvel Dice Game JavaScript</h3>
                          </a>
                      </div>
                      <div className="item">
                          <a href="https://argonautesmc.herokuapp.com" target="_blank" rel="noreferrer">
-                             <img alt="" src="images/ARGO2.jpg" />
+                             <img alt="" src="images/argo2.jpeg" />
                              <h3>Create/Read PHP</h3>
                          </a>
                      </div>
